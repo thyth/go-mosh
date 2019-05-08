d6897ba7ab54e389f904f52ebea76fb801cae02f
Daniel Selifonov <ds@thyth.com>
2019-05-07 23:24:38 -0700

    Working toward automation of the Go wrapper build

--- pkg/mosh/util.go
+++ pkg/mosh/util.go
@@ -5,3 +5,5 @@ import "time"
 func Timestamp(t time.Time) uint64 {
 	return uint64(t.UnixNano() / int64(time.Millisecond/time.Nanosecond))
 }
+
+// TODO find a way to read the VERSION from the associated build of Mosh and make it available as a constant here
