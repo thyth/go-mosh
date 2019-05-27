12c5d18af36a3fddd731db9eab41278b0d94311e
Daniel Selifonov <ds@thyth.com>
2019-05-26 23:30:07 -0700

    Reconsolidating SWIG specifications into one top-level Mosh spec
    There are otherwise seemingly unresolvable linking issues when the
    spec is split into three sub-modules.
    
    Require re-writing the pkg/ wrappers to match the new top-level mosh
    internal package.

--- internal/mosh/build.go
+++ internal/mosh/build.go
@@ -1,3 +1,8 @@
 package mosh
 
-// Dummy file to make sure sub-packages are reachable by 'go build'
+// Per https://github.com/golang/go/issues/6696 -- use a build.go file to specify import "C"/cgo pragmas for SWIG
+// generated wrappers. To build this project, there are fairly involved additional includes/linking requirements.
+
+// #cgo CXXFLAGS: -std=c++11 -I ${SRCDIR}/../../mosh/ -I ${SRCDIR}/../../mosh/src/crypto -I ${SRCDIR}/../../mosh/src/frontend -I ${SRCDIR}/../../mosh/src/network -I ${SRCDIR}/../../mosh/src/protobufs -I ${SRCDIR}/../../mosh/src/statesync -I ${SRCDIR}/../../mosh/src/terminal -I ${SRCDIR}/../../mosh/src/util
+// #cgo LDFLAGS: -lncurses -lprotobuf ${SRCDIR}/../../mosh/src/crypto/base64.o ${SRCDIR}/../../mosh/src/crypto/crypto.o ${SRCDIR}/../../mosh/src/crypto/ocb.o ${SRCDIR}/../../mosh/src/frontend/terminaloverlay.o ${SRCDIR}/../../mosh/src/network/network.o ${SRCDIR}/../../mosh/src/protobufs/hostinput.pb.o ${SRCDIR}/../../mosh/src/statesync/completeterminal.o ${SRCDIR}/../../mosh/src/terminal/parser.o ${SRCDIR}/../../mosh/src/terminal/parseraction.o ${SRCDIR}/../../mosh/src/terminal/parserstate.o ${SRCDIR}/../../mosh/src/terminal/terminal.o ${SRCDIR}/../../mosh/src/terminal/terminaldispatcher.o ${SRCDIR}/../../mosh/src/terminal/terminaldisplay.o ${SRCDIR}/../../mosh/src/terminal/terminaldisplayinit.o ${SRCDIR}/../../mosh/src/terminal/terminalframebuffer.o ${SRCDIR}/../../mosh/src/terminal/terminalfunctions.o ${SRCDIR}/../../mosh/src/terminal/terminaluserinput.o ${SRCDIR}/../../mosh/src/util/timestamp.o
+import "C"
