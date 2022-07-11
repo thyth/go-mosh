084b5325a4d1b94610351ca34bcd09f54bde6683
Daniel Selifonov <ds@thyth.com>
2022-07-10 21:00:05 -0700

    Updated mosh upstream to 581bd67c5ad51cb59176472101f6a0b03703e494
    This appears necessary to build on macOS Monterey with go 1.18
    toolchain. Some paths have changed within mosh. Added a fixup
    script which needs to be run after mosh is built. Mosh can now
    be built by invoking the build-mosh script, which should work after
    the mosh git submodule is initialized.

--- pkg/mosh/terminal/terminal.go
+++ pkg/mosh/terminal/terminal.go
@@ -1,6 +1,6 @@
 /*
  * go-mosh: mosh SWIG wrapper for Golang
- * Copyright 2019 Daniel Selifonov
+ * Copyright 2019-2022 Daniel Selifonov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package terminal
 
 import (
