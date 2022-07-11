19ac07df254decf9ffc732db7c1f20c603a64857
Daniel Selifonov <ds@thyth.com>
2022-07-10 21:32:00 -0700

    Adjust includes paths for mosh's config.h
    Removes need to hack this in build-fixup.sh

--- build-fixup.sh
+++ build-fixup.sh
@@ -5,8 +5,6 @@ pushd . || exit 1
 cd "$(dirname $0)" || exit 1
 
 # fix-ups to mosh for building as a library
-echo "Fixing up shared.h"
-sed -i_ 's/#include "config.h"/#include "..\/include\/config.h"/' mosh/src/util/shared.h
 
 # macOS (on case insensitive filesystems) chokes when building via go tools if this file is present --
 # see https://github.com/mobile-shell/mosh/issues/1140 -- this was fixed within mosh itself for its own build, but not
